e38598f197824073eda5666fed000756a9cf878b
Flix21 <thefflix@gmail.com>
2019-05-28 16:23:14 +0200

    Inicio del proyecto

---
+++ RPG-App/src/com/macaco/Main.java
@@ -0,0 +1,8 @@
+package com.macaco;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Aquí hay que crear la conexion con la base de datos,crear un objeto partida, y empezar con la logica del juego.
+    }
+}
