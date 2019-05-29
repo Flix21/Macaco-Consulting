e2c1521c13c61c3514683abab0a23b92b1632c93
TheGuayeah <37774329+TheGuayeah@users.noreply.github.com>
2019-05-29 12:38:23 +0200

    inicio del juego

--- RPG-App/src/com/macaco/Main.java
+++ RPG-App/src/com/macaco/Main.java
@@ -1,16 +1,74 @@
 package com.macaco;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main
 {
     private static Personaje personaje;
+
+    private enum path { NONE, CASTILLO, BOSQUE, CUEVA };
+    private static path camino;
+    private static List<String> enemy, estancia;
 
     public static void main(String[] args)  throws InterruptedException /*Excepción necesaria para calcular el tiempo*/
     {
         if(personaje == null)
         {
             createPlayer();
+        }
+        choosePath();
+
+    }
+
+    private static void choosePath()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println(personaje.getNombre() + ", ha estado vagando por la ruta comercial mucho tiempo sin destino.\n" +
+                            "Tras meses de viaje se encuentra con un poblado, es momento de empezar una nueva vida y\n" +
+                            "busca el gremio más popular para emprender su primera aventura y hacerse conocer como\n" +
+                            " un cavallero honorable y honrado.\n\n" +
+                            "En el tablón del gremio se encuentra con tres carteles de misiones que aún no han sido asignadas:\n" +
+                            "[1] CASTILLO ENCANTADO     [2] BOSQUE MALDITO      [3] CUEVA TENEBROSA");
+        switch (keyboard.nextInt())
+        {
+            case 1:
+                camino = path.CASTILLO;
+                break;
+            case 2:
+                camino = path.BOSQUE;
+                break;
+            case 3:
+                camino = path.CUEVA;
+                break;
+            default:
+                camino = path.NONE;
+                break;
+        }
+        switch (camino)
+        {
+            case CASTILLO:
+                enemy  = Arrays.asList("VAMPIRO", "CABALLERO HUECO", "FANTASMA", "ESQUELETO");
+                estancia  = Arrays.asList("PASILLO", "SALÓN", "PATIO", "TORRE", "MAZMORRA");
+                break;
+            case BOSQUE:
+                enemy  = Arrays.asList("GOBLIN", "TROLL", "FANTASMA", "LICÁNTROPO");
+                estancia  = Arrays.asList("CIÉNAGA", "CLARO", "ESPESURA", "SANTUARIO");
+                break;
+            case CUEVA:
+                enemy  = Arrays.asList("GOBLIN", "TROLL", "OSO SALVAJE", "ESQUELETO");
+                estancia  = Arrays.asList("ENTRADA", "GALERÍA", "GALERÍA OSCURA", "GALERÍA DE LA LUNA");
+                break;
+        }
+
+        System.out.println("Arrancas el trozo de pergamino del tablón y en él ves una advertencia:");
+        System.out.println("Éstos son los enemigos con los que te puedes encontrar");
+
+        for (int i = 0; i < enemy.size(); i++)
+        {
+            if(i < 3)
+                System.out.print(enemy.get(i) + ", ");
+            else
+                System.out.print(enemy.get(i));
         }
     }
 
@@ -37,7 +95,7 @@ public class Main
         {
             System.out.println("¿Cuántos puntos de los " + puntos + " quieres añadirle a " + atributos[i] + "?");
             Scanner keyboard = new Scanner(System.in);
-            String puntos_agregados =keyboard.nextLine();
+            String puntos_agregados = keyboard.nextLine();
             if(Integer.parseInt(puntos_agregados) <= puntos)
             {
                 puntos -= Integer.parseInt(puntos_agregados);
