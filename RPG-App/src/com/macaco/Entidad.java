5c4075446e0b6e7a13c92eb14a3b3700c6aaf08e
miguelangel94 <50964244+miguelangel94@users.noreply.github.com>
2019-05-29 12:48:40 +0200

    dsadsad
    sadsds

---
+++ RPG-App/src/com/macaco/Entidad.java
@@ -0,0 +1,74 @@
+package com.macaco;
+
+public abstract class Entidad {
+    public String nombre ="";
+    public int ataque;
+    public int defensa;
+    public int velocidad;
+    public int salud=100;
+
+
+    // constructor vacio
+    public Entidad (){
+
+    }
+    //Constructor lleno
+    public Entidad(String nombre, int ataque, int defensa, int velocidad, int puntos_salud) {
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.velocidad = velocidad;
+        this.salud = puntos_salud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int puntos_salud) {
+        this.salud = puntos_salud;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidad{" +
+                "nombre='" + nombre + '\'' +
+                ", ataque=" + ataque +
+                ", defensa=" + defensa +
+                ", velocidad=" + velocidad +
+                ", puntos_salud=" + salud +
+                '}';
+    }
+}
